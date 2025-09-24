e70d5ad9a10384624ee706ded5b8f00a588a714d
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2025-09-24 18:49:30 +0200

    Update App.xaml.cs

--- src/Plmeco.App/App.xaml.cs
+++ src/Plmeco.App/App.xaml.cs
@@ -4,6 +4,14 @@ namespace Plmeco.App
 {
     public partial class App : Application
     {
+        public App()
+        {
+            this.DispatcherUnhandledException += (s, e) =>
+            {
+                MessageBox.Show("Ha ocurrido un error: " + e.Exception.Message,
+                                "PLMECO", MessageBoxButton.OK, MessageBoxImage.Error);
+                e.Handled = true; // evita que la app se cierre
+            };
+        }
     }
 }
-
