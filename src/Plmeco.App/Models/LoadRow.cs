93484fa6832b0052e1b32606a731e29af6b044fb
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2025-09-24 18:27:08 +0200

    Update LoadRow.cs

--- src/Plmeco.App/Models/LoadRow.cs
+++ src/Plmeco.App/Models/LoadRow.cs
@@ -1,13 +1,15 @@
+using System;
+
 namespace Plmeco.App.Models;
 
 public class LoadRow
 {
     public int Id { get; set; }
-    public string Transportista { get; set; }
-    public string Matricula { get; set; }
-    public string Destino { get; set; }
-    public string Muelle { get; set; }
-    public string Estado { get; set; }
+    public string Transportista { get; set; } = "";
+    public string Matricula { get; set; } = "";
+    public string Destino { get; set; } = "";
+    public string Muelle { get; set; } = "";
+    public string Estado { get; set; } = "";
     public TimeSpan? LlegadaReal { get; set; }
     public TimeSpan? SalidaReal { get; set; }
     public TimeSpan? SalidaTope { get; set; }
