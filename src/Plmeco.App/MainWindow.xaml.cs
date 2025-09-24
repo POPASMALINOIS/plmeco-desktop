2e4a5f6ec1881da26255f047918685220fda4e4d
POPASMALINOIS <j.m.p.garrido87@outlook.es>
2025-09-24 18:25:43 +0200

    Update MainWindow.xaml.cs

--- src/Plmeco.App/MainWindow.xaml.cs
+++ src/Plmeco.App/MainWindow.xaml.cs
@@ -1,40 +1,42 @@
+using System;
+using System.Collections.ObjectModel;
+using System.Windows;
 using Microsoft.Win32;
 using Plmeco.App.Models;
 using Plmeco.App.Services;
-using System.Collections.ObjectModel;
-using System.Windows;
 
-namespace Plmeco.App;
-
-public partial class MainWindow : Window
+namespace Plmeco.App
 {
-    public ObservableCollection<LoadRow> Rows { get; set; } = new();
-
-    public MainWindow()
+    public partial class MainWindow : Window
     {
-        InitializeComponent();
-        dgCargas.ItemsSource = Rows;
-    }
+        public ObservableCollection<LoadRow> Rows { get; set; } = new();
 
-    private void Importar_Click(object sender, RoutedEventArgs e)
-    {
-        var dlg = new OpenFileDialog { Filter = "Excel Files|*.xlsx;*.xls" };
-        if (dlg.ShowDialog() == true)
+        public MainWindow()
         {
-            var data = ImportService.ImportExcel(dlg.FileName);
-            Rows.Clear();
-            foreach (var row in data) Rows.Add(row);
+            InitializeComponent();
+            dgCargas.ItemsSource = Rows;
         }
-    }
 
-    private void DataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
-    {
-        if (dgCargas.CurrentItem is LoadRow row)
+        private void Importar_Click(object sender, RoutedEventArgs e)
         {
-            var col = dgCargas.CurrentColumn.Header.ToString();
-            if (col == "LLEGADA REAL") row.LlegadaReal = DateTime.Now.TimeOfDay;
-            if (col == "SALIDA REAL") row.SalidaReal = DateTime.Now.TimeOfDay;
-            dgCargas.Items.Refresh();
+            var dlg = new OpenFileDialog { Filter = "Excel Files|*.xlsx;*.xls" };
+            if (dlg.ShowDialog() == true)
+            {
+                var data = ImportService.ImportExcel(dlg.FileName);
+                Rows.Clear();
+                foreach (var row in data) Rows.Add(row);
+            }
+        }
+
+        private void DataGrid_MouseDoubleClick(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            if (dgCargas.CurrentItem is LoadRow row)
+            {
+                var col = dgCargas.CurrentColumn.Header.ToString();
+                if (col == "LLEGADA REAL") row.LlegadaReal = DateTime.Now.TimeOfDay;
+                if (col == "SALIDA REAL") row.SalidaReal = DateTime.Now.TimeOfDay;
+                dgCargas.Items.Refresh();
+            }
         }
     }
 }
